f9efb1d700242779915e9679b5bc8e7b3919a779
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-14 00:23:51 +0530

    created SpeedConversionScreen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/screens/speed_conversion_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SpeedConversionScreen extends StatefulWidget {
+  static const routeName = '/speed-conversion-screen';
+  const SpeedConversionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SpeedConversionScreen> createState() => _SpeedConversionScreenState();
+}
+
+class _SpeedConversionScreenState extends State<SpeedConversionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
