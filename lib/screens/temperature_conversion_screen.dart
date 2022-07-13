89d78582840693107f5f8b18da1520d5981b61ee
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-14 00:25:26 +0530

    created TemperatureConversionScreen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/screens/temperature_conversion_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TemperatureConversionScreen extends StatefulWidget {
+  static const routeName = '/temperature-conversion-screen';
+  const TemperatureConversionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TemperatureConversionScreen> createState() =>
+      _TemperatureConversionScreenState();
+}
+
+class _TemperatureConversionScreenState
+    extends State<TemperatureConversionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
