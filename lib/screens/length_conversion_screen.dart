f059ad9a5d2df10f9a52556950b830049f53bd27
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-14 00:05:26 +0530

    created LengthConversionScreen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/screens/length_conversion_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LengthConversionScreen extends StatefulWidget {
+  static const routeName = '/length-conversion-screen';
+  const LengthConversionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LengthConversionScreen> createState() => _LengthConversionScreenState();
+}
+
+class _LengthConversionScreenState extends State<LengthConversionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
