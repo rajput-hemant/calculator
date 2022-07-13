69d849a9d3fa14b51a242ebfdfa721edf6b9b8a6
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-14 00:16:26 +0530

    created AreaConversionScreen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/screens/area_conversion_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AreaConversionScreen extends StatefulWidget {
+  static const routeName = '/area-conversion-screen';
+  const AreaConversionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AreaConversionScreen> createState() => _AreaConversionScreenState();
+}
+
+class _AreaConversionScreenState extends State<AreaConversionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
