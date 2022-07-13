0d138f31c6e7b7d4b656ec60998e6241b9906ad1
Khushal-ag <starkhush5@gmail.com>
2022-07-14 02:59:14 +0530

    added volume conversion functionality

---
+++ lib/screens/unit_converter_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'area_conversion_screen.dart';
+import 'exchange_rate_screen.dart';
+import 'length_conversion_screen.dart';
+import '../widgets/unit_box.dart';
+import '../widgets/unit_icon.dart';
+import 'power_conversion_screen.dart';
+import 'pressure_conversion_screen.dart';
+import 'speed_conversion_screen.dart';
+import 'temperature_conversion_screen.dart';
+import 'volume_conversion_screen.dart';
+import 'weigth_conversion_screen.dart';
+
+class UnitConverterScreen extends StatelessWidget {
+  static const routeName = '/unit-converter';
+
+  const UnitConverterScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF060606),
+      appBar: AppBar(
+        title: const Text(
+          "Unit converter",
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.globe, textData: 'Currency'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, ExchangeRateScreen.routeName),
+                ),
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.ruler, textData: 'Length'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, LengthConversionScreen.routeName),
+                ),
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.chartArea, textData: 'Area'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, AreaConversionScreen.routeName),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.gaugeHigh, textData: 'Speed'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, SpeedConversionScreen.routeName),
+                ),
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.weightHanging,
+                      textData: 'Weight'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, WeightConversionScreen.routeName),
+                ),
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.temperatureThreeQuarters,
+                      textData: 'Temperature'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, TemperatureConversionScreen.routeName),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.bolt, textData: 'Power'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, PowerConversionScreen.routeName),
+                ),
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.gaugeSimple,
+                      textData: 'Pressure'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, PressureConversionScreen.routeName),
+                ),
+                UnitBox(
+                  icondata: const UnitIcon(
+                      iconName: FontAwesomeIcons.cube, textData: 'Volume'),
+                  iconfunction: () => Navigator.pushNamed(
+                      context, VolumeConversionScreen.routeName),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/volume_conversion_screen.dart
+++ lib/screens/volume_conversion_screen.dart
@@ -1,3 +1,7 @@
+import 'package:calculator/models/volume.dart';
+import 'package:calculator/widgets/bottom_sheet_tile.dart';
+import 'package:calculator/widgets/field_list_tile.dart';
+import 'package:calculator/widgets/keypad_builder.dart';
 import 'package:flutter/material.dart';
 
 class VolumeConversionScreen extends StatefulWidget {
@@ -9,8 +13,221 @@ class VolumeConversionScreen extends StatefulWidget {
 }
 
 class _ConversionScreenState extends State<VolumeConversionScreen> {
+  final _volumeList = Volume.volume;
+  bool _isFirstField = true;
+  int _firstFieldIndex = 0, _secondFieldIndex = 1;
+  dynamic _firstField = '0', _secondField = '0';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFF060606),
+      appBar: AppBar(
+        title: const Text('Volume Conversion'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  FieldListTile(
+                    field: _firstField,
+                    fieldTitle: _volumeList[_firstFieldIndex].name,
+                    index: _firstFieldIndex,
+                    list: _volumeList,
+                    bottomSheetHeader: "Select Unit",
+                    onTappingField: () {
+                      _isFirstField = true;
+                      _secondField = '0';
+                    },
+                    child: ListView.builder(
+                      itemCount: _volumeList.length,
+                      itemBuilder: (context, i) {
+                        return BottomSheetTile(
+                          index: i,
+                          list: _volumeList,
+                          onSelecting: () {
+                            setState(() => _firstFieldIndex = i);
+                            Navigator.pop(context);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  const Divider(
+                    height: 15,
+                    color: Colors.grey,
+                  ),
+                  FieldListTile(
+                    field: _secondField,
+                    fieldTitle: _volumeList[_secondFieldIndex].name,
+                    index: _secondFieldIndex,
+                    list: _volumeList,
+                    bottomSheetHeader: "Select Unit",
+                    onTappingField: () {
+                      _isFirstField = false;
+                      _firstField = '0';
+                    },
+                    child: ListView.builder(
+                      itemCount: _volumeList.length,
+                      itemBuilder: (context, i) {
+                        return BottomSheetTile(
+                          index: i,
+                          list: _volumeList,
+                          onSelecting: () {
+                            setState(() => _secondFieldIndex = i);
+                            Navigator.pop(context);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  const Divider(
+                    height: 15,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          KeyPad(
+            onPressed0: () => onPressed(strToConcate: "0"),
+            onPressed1: () => onPressed(strToConcate: "1"),
+            onPressed2: () => onPressed(strToConcate: "2"),
+            onPressed3: () => onPressed(strToConcate: "3"),
+            onPressed4: () => onPressed(strToConcate: "4"),
+            onPressed5: () => onPressed(strToConcate: "5"),
+            onPressed6: () => onPressed(strToConcate: "6"),
+            onPressed7: () => onPressed(strToConcate: "7"),
+            onPressed8: () => onPressed(strToConcate: "8"),
+            onPressed9: () => onPressed(strToConcate: "9"),
+            onPressed00: () => onPressed(strToConcate: "00"),
+            onPressedDot: () => decimalButton(),
+            onPressedDel: deleteButton,
+            onPressedClear: clearButton,
+            onPressedConvert: convertButton,
+          )
+        ],
+      ),
+    );
+  }
+
+  void convert({
+    required int from,
+    required int to,
+    required double amount,
+  }) {
+    var result = (amount / _volumeList[from].rate) * _volumeList[to].rate;
+    setState(() {
+      if (_isFirstField) {
+        _secondField = result.toStringAsFixed(2);
+      } else {
+        _firstField = result.toStringAsFixed(2);
+      }
+    });
+  }
+
+  void convertButton() {
+    return setState(() {
+      if (_isFirstField) {
+        convert(
+          from: _firstFieldIndex,
+          to: _secondFieldIndex,
+          amount: double.parse(_firstField),
+        );
+      } else {
+        convert(
+          from: _secondFieldIndex,
+          to: _firstFieldIndex,
+          amount: double.parse(_secondField),
+        );
+      }
+    });
+  }
+
+  void decimalButton() {
+    return setState(() {
+      if (_isFirstField && !_firstField.toString().contains(".")) {
+        _firstField += '.';
+      } else if (!_isFirstField && !_secondField.toString().contains(".")) {
+        _secondField += '.';
+      }
+    });
+  }
+
+  void clearButton() {
+    return setState(
+      () {
+        _firstField = '0';
+        _secondField = '0';
+      },
+    );
+  }
+
+  void deleteButton() {
+    return setState(() {
+      if (_isFirstField) {
+        if (_firstField.length == 1) {
+          _firstField = '0';
+          _secondField = '0';
+        } else {
+          _firstField = _firstField.substring(0, _firstField.length - 1);
+        }
+      } else {
+        if (_secondField.length == 1) {
+          _firstField = '0';
+          _secondField = '0';
+        } else {
+          _secondField = _secondField.substring(0, _secondField.length - 1);
+        }
+      }
+    });
+  }
+
+  void onPressed({String? strToConcate}) {
+    return setState(
+      () {
+        if (_isFirstField) {
+          if (_firstField == '0.0') {
+            _firstField = strToConcate!;
+          } else {
+            if (strToConcate == "00") {
+              _firstField = (int.parse(_firstField) * 100).toString();
+            } else if (strToConcate == '0') {
+              _firstField = (int.parse(_firstField) * 10).toString();
+            } else {
+              if (_firstField.toString().contains('.')) {
+                _firstField =
+                    (double.parse(_firstField + strToConcate!)).toString();
+              } else {
+                _firstField =
+                    (int.parse(_firstField + strToConcate!)).toString();
+              }
+            }
+          }
+        } else {
+          if (_secondField == '0.0') {
+            _secondField = strToConcate!;
+          } else {
+            if (strToConcate == "00") {
+              _secondField = (int.parse(_secondField) * 100).toString();
+            } else if (strToConcate == '0') {
+              _secondField = (int.parse(_secondField) * 10).toString();
+            } else {
+              if (_firstField.toString().contains('.')) {
+                _secondField =
+                    (double.parse(_secondField + strToConcate!)).toString();
+              } else {
+                _secondField =
+                    (int.parse(_secondField + strToConcate!)).toString();
+              }
+            }
+          }
+        }
+      },
+    );
   }
 }
