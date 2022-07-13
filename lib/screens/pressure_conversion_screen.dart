fd0cd46febbcbc6d21f342aaf4952b3e539b5ce8
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-14 00:25:56 +0530

    created PressureConversionScreen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/screens/pressure_conversion_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PressureConversionScreen extends StatefulWidget {
+  static const routeName = '/pressure-conversion-screen';
+  const PressureConversionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PressureConversionScreen> createState() =>
+      _PressureConversionScreenState();
+}
+
+class _PressureConversionScreenState extends State<PressureConversionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
