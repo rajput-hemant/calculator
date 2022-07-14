8cd49af558bc16051a40bb20a80700d95e7b4caa
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-15 02:39:53 +0530

    updated SettingsScreen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -7,53 +7,23 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Settings',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 22,
-              fontFamily: 'Roboto',
-            ),
-          ),
-          // backgroundColor: Colors.black,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          'Settings',
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Colors.black,
-        body: SafeArea(
-            child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  margin: const EdgeInsets.only(left: 22, top: 25),
-                  child: const Text(
-                    'Version',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                )
-              ],
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              margin: const EdgeInsets.only(left: 24, top: 5),
-              child: const Text(
-                '12.0.14',
-                style: TextStyle(color: Colors.grey),
-              ),
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Container(
-                margin: const EdgeInsets.only(left: 22, top: 20),
-                child: const Text('Help & Feedback',
-                    style: TextStyle(fontSize: 18, color: Colors.white)),
-              ),
-              Container(
-                margin: const EdgeInsets.only(right: 8, top: 20),
-                child: const Icon(Icons.navigate_next, color: Colors.grey),
-              )
-            ])
-          ],
-        )));
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8),
+        child: ListTile(
+          title: Text(
+            'Version',
+            style: TextStyle(fontSize: 18),
+          ),
+          subtitle: Text('0.1.0'),
+        ),
+      ),
+    );
   }
 }
